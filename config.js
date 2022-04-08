0549b7c5969a5b399d738e3dd7f71a991d7e5d11
Kyouka Tachibana <91820453+Yuri-Neko@users.noreply.github.com>
2022-04-08 20:40:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281227144597', 'ozan', true],
+  ['6282112080081', 'Kyouko', true],
   //['6281227144597']
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
-global.prems = ['6282317110606'] // Premium user has unlimited limit
+global.prems = ['6282112080081'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
