41d943e576d857411976bf3323856f22462c5699
Kyouka Tachibana <91820453+Yuri-Neko@users.noreply.github.com>
2022-04-09 19:12:25 +0700

    Update sora.js

--- plugins/sora.js
+++ plugins/sora.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text }) => {
-  let url = 'https://i.pinimg.com/originals/9b/bd/d3/9bbdd3c7884308f36df49d3a3b2eb6f7.gif'
+  let url = 'https://i.pinimg.com/736x/15/87/11/158711b8c438f716ab2f16a5780b09d1.jpg'
   conn.sendFile(m.chat, url, '', '', m, false, { asSticker: true })
 }
 handler.command = ['sora'] // buat command
